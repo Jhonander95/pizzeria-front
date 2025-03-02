22e35ea48b0c8cc3098036a8ab60584b8850fb39
Jhonander <tellezjhonander@gmail.com>
2025-03-02 15:59:41 -0500

    fix bug into url login

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -10,6 +10,8 @@ export class AuthService {
   private currentUserSubject: BehaviorSubject<any>;
   public currentUser: Observable<any>;
 
+  private apiUrl = 'http://localhost:8080/admin';
+
   constructor(private http: HttpClient) {
     this.currentUserSubject = new BehaviorSubject<any>(JSON.parse(localStorage.getItem('currentUser') || '{}'));
     this.currentUser = this.currentUserSubject.asObservable();
@@ -20,14 +22,14 @@ export class AuthService {
   }
 
   login(username: string, password: string) {
-    return this.http.post<any>(`/api/auth/login`, { username, password })
+    return this.http.post<any>(`http://localhost:8080/auth/login`, { username, password })
       .pipe(map(user => {
         localStorage.setItem('currentUser', JSON.stringify(user));
         this.currentUserSubject.next(user);
         return user;
       }));
   }
-
+ 
   logout() {
     localStorage.removeItem('currentUser');
     this.currentUserSubject.next(null);
