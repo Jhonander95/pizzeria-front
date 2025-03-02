0f16dbd09985c9da6d32e836ab06050d641ca7e7
jhonander95 <tellezjhonander@gmail.com>
2025-03-02 07:38:19 -0400

    Starting

--- src/app/core/models/product.model.ts
+++ src/app/core/models/product.model.ts
@@ -1,5 +1,5 @@
 export interface Product {
   id?: number;
-  name: string;
-  price: number;
+  nombre: string;
+  precio: number;
 }
--- src/app/features/products/product-list/product-list.component.ts
+++ src/app/features/products/product-list/product-list.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Product } from '../../../core/models/product.model';
+import { ProductService } from '../../../core/services/product.service';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-product-list',
   standalone: true,
-  imports: [],
+  imports: [NgFor ],
   templateUrl: './product-list.component.html',
   styleUrl: './product-list.component.scss'
 })
-export class ProductListComponent {
+export class ProductListComponent implements OnInit {
+  products: Product[] = [];
 
+  constructor(private productService: ProductService) {}
+
+  ngOnInit() {
+    this.productService.getProducts().subscribe((data) => {
+      this.products = data;
+    });
+  }
 }
