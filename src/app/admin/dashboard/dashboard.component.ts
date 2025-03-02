3d6f3f46765b159656d46e7c8e76fcc646f729a7
Jhonander <tellezjhonander@gmail.com>
2025-03-02 16:47:33 -0500

    Backup befor apdate angular

--- src/app/admin/dashboard/dashboard.component.ts
+++ src/app/admin/dashboard/dashboard.component.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../../core/services/auth.service';
+import { RouterLinkActive } from '@angular/router';
+import { RouterLink } from '@angular/router';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
+  imports: [RouterLinkActive, RouterModule],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
 export class DashboardComponent {
+
+  constructor(
+    private router: Router,
+    private authService: AuthService
+  ) {}
+
+  logout() {
+    this.authService.logout();
+    this.router.navigate(['/admin/login']);
+  }
 
 }
